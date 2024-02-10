4a5f7b38e52205be9e40c0ad4e4b6b4a087fad78
¨sktbrd¨ <sktbrd.eth@gmail.com>
2024-02-10 14:21:43 -0500

    Maya Genesis

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 "use client";
-import { Tooltip, Box, Text, VStack, Spinner, Center, Button, Flex, useDisclosure, Modal, ModalBody, ModalOverlay, ModalHeader, ModalContent, ModalCloseButton, Input, ModalFooter } from "@chakra-ui/react";
+import { Tooltip, Box, Text, HStack, VStack, Spinner, Center, Button, Flex, useDisclosure, Modal, ModalBody, ModalOverlay, ModalHeader, ModalContent, ModalCloseButton, Input, ModalFooter } from "@chakra-ui/react";
 import Image from "next/image";
 import styles from "./page.module.css";
 import Wallet from "./wallet";
 import { useState, useEffect } from "react";
 import { formatCacao } from "@/utils/formatBigInt";
 import { useHandleTransfer } from './hooks/useHandleTransfer'; // Adjust the path as needed
-import { FaCopy } from 'react-icons/fa';
-import { keyframes } from '@emotion/react';
-
+import { FaCopy, FaLock } from 'react-icons/fa';
+import { AnyNsRecord } from "dns";
+import { useToast } from "@chakra-ui/react";
+// import chakra provider 
+import { ChakraProvider } from "@chakra-ui/react";
 
 // Define interface for a single balance item
 interface BalanceItem {
@@ -61,6 +63,7 @@ export default function Home() {
     const [destination, setDestination] = useState("");
     const [selectedBalance, setSelectedBalance] = useState<BalanceItem | null>(null);
     const handleTransfer = useHandleTransfer(keepKey);
+    const toast = useToast();
 
     if (!keepKey) {
         return (
@@ -80,123 +83,153 @@ export default function Home() {
         }
     };
 
+
+    // create a handle Copy function that copy the value to the clipboard
+    const handleCopy = (value: string) => {
+        navigator.clipboard.writeText(value);
+        toast({
+            title: "Address copied",
+            description: "The wallet address has been copied to your clipboard.",
+            status: "success",
+            duration: 3000,
+            isClosable: true,
+            position: "bottom-left", // Change this to any valid position
+            variant: "subtle", // Change this to any valid variant
+        });
+    }
+
+
+
     // Test Wallet : maya1g9el7lzjwh9yun2c4jjzhy09j98vkhfxfqkl5k
 
 
     return (
-        <>
-            <main className={styles.main}>
-                <div className={styles.description}>
-                    <VStack spacing={4} align="stretch" justify="center" width="100%" >
-                        <center>
-                            <Image
-                                src={"/maya.jpg"}
-                                alt="Maya Logo"
-                                width={200}
-                                height={200}
-                                style={{
-                                    borderRadius: "50%",
+        <ChakraProvider >
+            <>
+                <main className={styles.main}>
+                    <div className={styles.description}>
+                        <VStack spacing={4} align="stretch" justify="center" width="100%" >
+                            <center>
+                                <Image
+                                    src={"/maya.jpg"}
+                                    alt="Maya Logo"
+                                    width={200}
+                                    height={200}
+                                    style={{
+                                        borderRadius: "50%",
 
-                                }}
-                            />
-                        </center>
-                        {Object.entries(keepKey).map(([currency, data]) => (
-                            <Box key={currency} p={5} shadow="md" borderWidth="1px" borderRadius="lg" >
+                                    }}
+                                />
+                            </center>
+                            {Object.entries(keepKey).map(([currency, data]) => (
+                                <Box key={currency} p={5} shadow="md" borderWidth="1px" borderRadius="lg" >
+                                    <center>
+                                        <Text fontSize="2xl" fontWeight="bold" mb={3}>{currency}</Text>
+                                    </center>
+                                    <HStack>
+                                        <Text fontSize="md">
+                                            Address: {data.wallet.address}
+                                        </Text>
+                                        <FaCopy onClick={() => handleCopy(data.wallet.address)} />
+                                    </HStack>
+                                    {
+                                        data.wallet.balance.map((balance, index) => (
+                                            <Flex key={index} justify="space-between">
+                                                <Text fontSize="md" mt={1}>
+                                                    Balance: {formatCacao(balance.bigIntValue, balance.decimalMultiplier)} {balance.symbol}
+                                                </Text>
+                                                <Button
+                                                    onClick={() => {
+                                                        setSelectedBalance(balance);
+                                                        setIsSendFormVisible(true);
+                                                    }}
+                                                    borderRadius={5}
+                                                    p={2}
+                                                    backgroundColor={"black"}
+                                                    color={"white"}
+                                                >
+                                                    SEND {balance.symbol}
+                                                </Button>
+                                            </Flex>
+                                        ))
+                                    }
+                                </Box>
+                            ))}
+
+                            {isSendFormVisible && (
+
+
                                 <center>
-                                    <Text fontSize="2xl" fontWeight="bold" mb={3}>{currency}</Text>
+
+                                    <Box m={"10"} borderRadius="10px" mt={4} maxW={"80%"} border="2px solid black" >
+                                        <Flex justifyContent="center" alignItems="center" p={4} border="1px solid black"  >
+                                            <Box flex="1"></Box>
+                                            <Button color={"white"} bg={"red"} onClick={() => setIsSendFormVisible(false)}>
+                                                X
+                                            </Button>
+                                        </Flex>
+                                        <Flex p={4} flexDirection={"column"}>
+                                            <Text fontSize="xl" fontWeight="bold" mb={3}>
+                                                Send {selectedBalance?.symbol}
+                                            </Text>
+                                            <Flex justifyContent={"left"}>
+
+                                                <Button
+                                                    maxW={"30%"}
+                                                    m={2}
+                                                    borderRadius="5px"
+                                                    background={"black"}
+                                                    color={"white"}
+                                                >
+
+
+                                                    Max
+                                                </Button>
+                                            </Flex>
+
+                                            <Input
+                                                placeholder={"Amount to Send"}
+                                                value={amountToSend}
+                                                onChange={(e: any) => setAmountToSend(e.target.value)}
+                                                mb={3}
+                                                borderRadius={5}
+                                                marginLeft={2}
+                                            />
+                                            <Input
+                                                placeholder="maya...fxfqkl5k"
+                                                value={destination}
+                                                onChange={(e: any) => setDestination(e.target.value)}
+                                                borderRadius={5}
+                                                marginLeft={2}
+
+                                            />
+                                            <Flex justifyContent={"right"}>
+
+                                                <Button
+                                                    onClick={onClickSend}
+                                                    backgroundColor={"limegreen"}
+                                                    borderRadius={5}
+                                                    minW={"100px"}
+                                                    minH={"40px"}
+                                                    m={2}
+                                                    leftIcon={<FaLock />}
+
+
+                                                >
+                                                    Send
+                                                </Button>
+                                            </Flex>
+
+                                        </Flex>
+                                    </Box>
+
                                 </center>
 
-                                <Text fontSize="md">Address: {data.wallet.address}  <FaCopy onClick={() => console.log("create copy function here")} /></Text>
-                                {
-                                    data.wallet.balance.map((balance, index) => (
-                                        <Flex key={index} justify="space-between">
-                                            <Text fontSize="md" mt={1}>
-                                                Balance: {formatCacao(balance.bigIntValue, balance.decimalMultiplier)} {balance.symbol}
-                                            </Text>
-                                            <Button
-                                                onClick={() => {
-                                                    setSelectedBalance(balance);
-                                                    setIsSendFormVisible(true);
-                                                }}
-                                                borderRadius={5}
-                                                p={2}
-                                                backgroundColor={"black"}
-                                            >
-                                                SEND {balance.symbol}
-                                            </Button>
-                                        </Flex>
-                                    ))
-                                }
-                            </Box>
-                        ))}
-
-                        {isSendFormVisible && (
-
-
-                            <center>
-
-                                <Box m={"10"} border="1px solid white" borderRadius="10px" mt={4} maxW={"80%"}>
-                                    <Flex justifyContent="center" alignItems="center" p={4} >
-                                        <Box flex="1"></Box>
-                                        <Button color={"white"} bg={"transparent"} onClick={() => setIsSendFormVisible(false)}>
-                                            X
-                                        </Button>
-                                    </Flex>
-                                    <Flex p={4} flexDirection={"column"}>
-                                        <Text fontSize="xl" fontWeight="bold" mb={3}>
-                                            Send {selectedBalance?.symbol}
-                                        </Text>
-                                        <Flex justifyContent={"right"}>
-
-                                            <Button
-                                                maxW={"30%"}
-                                                m={10}
-                                                borderRadius="5px"
-                                                background={"black"}
-                                            >
-
-
-                                                Max
-                                            </Button>
-                                        </Flex>
-
-                                        <Input
-                                            placeholder={"Amount to Send"}
-                                            value={amountToSend}
-                                            onChange={(e) => setAmountToSend(e.target.value)}
-                                            mb={3}
-                                            borderRadius={5}
-                                        />
-                                        <Input
-                                            placeholder="maya...fxfqkl5k"
-                                            value={destination}
-                                            onChange={(e) => setDestination(e.target.value)}
-                                            borderRadius={5}
-
-                                        />
-                                        <Flex justifyContent={"right"}>
-
-                                            <Button
-                                                onClick={onClickSend}
-                                                backgroundColor={"limegreen"}
-                                                borderRadius={5}
-                                                minW={"100px"}
-                                                minH={"40px"}
-                                                m={10}
-                                            >
-                                                Send
-                                            </Button>
-                                        </Flex>
-
-                                    </Flex>
-                                </Box>
-
-                            </center>
-
-                        )}
-                    </VStack>
-                </div>
-            </main >
-        </>
+                            )}
+                        </VStack>
+                    </div>
+                </main >
+            </>
+        </ChakraProvider>
     );
 }
